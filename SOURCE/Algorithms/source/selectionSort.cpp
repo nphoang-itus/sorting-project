a03cec75aba1e15e5995a8ced5fd50248855a846
Nguyen Phuc Hoang <nphuchoang.itus@gmail.com>
2024-11-21 21:59:25 +0700

    Update Selection Sort

--- SOURCE/Algorithms/source/selectionSort.cpp
+++ SOURCE/Algorithms/source/selectionSort.cpp
@@ -1,9 +1,44 @@
+#include "../header/helpFunctions.hpp"
 #include "../header/selectionSort.hpp"
 
-void selectionSort(int array[], int n, long long &count_comparison) {
-
+template <class T>
+void selectionSort(std::vector<T>& array) {
+    for (size_t i = 0; i < array.size() - 1; ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < array.size(); ++j) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap<T>(array[i], array[minIndex]);
+    }
 }
 
-void selectionSort(int array[], int n) {
-
+template <class T>
+void selectionSort(std::vector<T> array, long long &countComparison) {
+    countComparison = 0;
+    for (size_t i = 0; ++countComparison && i < array.size() - 1; ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; ++countComparison && j < array.size(); ++j) {
+            if (++countComparison && array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap<T>(array[i], array[minIndex]);
+    }
 }
+
+// INSTANTIATION
+// selectionSort
+template void selectionSort(std::vector<int>&);
+template void selectionSort(std::vector<long long>&);
+template void selectionSort(std::vector<float>&);
+template void selectionSort(std::vector<double>&);
+template void selectionSort(std::vector<char>&);
+
+//selectionSort with Compare
+template void selectionSort(std::vector<int>, long long&);
+template void selectionSort(std::vector<long long>, long long&);
+template void selectionSort(std::vector<float>, long long&);
+template void selectionSort(std::vector<double>, long long&);
+template void selectionSort(std::vector<char>, long long&);
