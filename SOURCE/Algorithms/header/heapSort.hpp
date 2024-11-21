38115833b2d4b493a94999a183b0044c0fa406ee
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-21 23:46:07 +0700

    Add file header of heapSort

--- SOURCE/Algorithms/header/heapSort.hpp
+++ SOURCE/Algorithms/header/heapSort.hpp
@@ -1,8 +1,20 @@
 #pragma once
 #ifndef HEAP_SORT_HPP
 #define HEAP_SORT_HPP
+#include "../../myLibrary.hpp"
+#include "../../Others/header/helpFunctions.hpp"
 
-void heapSort(int array[], int n, long long &count_comparison);
-void heapSort(int array[], int n);
+
+template<class T>
+void heapify(std::vector<T>& array, int n, int i, long long& count_comparison);
+
+template<class T>
+void heapSort(std::vector<T>& array, int n, long long& count_comparison);
+
+template<class T>
+void heapify(std::vector<T>& array, int n, int i);
+
+template<class T>
+void heapSort(std::vector<T>& array, int n);
 
 #endif // HEAP_SORT_HPP
