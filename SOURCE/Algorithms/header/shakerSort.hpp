30437b77d808e82a6d6fc88aae6c1fb81d8c11a5
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-21 23:41:45 +0700

    Add file header of shakerSort

--- SOURCE/Algorithms/header/shakerSort.hpp
+++ SOURCE/Algorithms/header/shakerSort.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #ifndef SHAKER_SORT_HPP
 #define SHAKER_SORT_HPP
+#include "../../myLibrary.hpp"
+#include "../../Others/header/helpFunctions.hpp"
 
-void shakerSort(int array[], int n, long long &count_comparison);
-void shakerSort(int array[], int n);
+template<class T>
+void shakerSort(std::vector<T>& array, int n);
+
+template<class T>
+void shakerSort(std::vector<T>& array, int n, long long& count_comparison);
 
 #endif // SHAKER_SORT_HPP
