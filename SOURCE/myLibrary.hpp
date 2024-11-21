093a708a75b8ff1540f9a881c6b42191ef860fcb
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-21 23:38:43 +0700

    Add file header have all library

---
+++ SOURCE/myLibrary.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef MY_LIBRARY_HPP
+#define MY_LIBRARY_HPP
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <string>
+
+#endif
