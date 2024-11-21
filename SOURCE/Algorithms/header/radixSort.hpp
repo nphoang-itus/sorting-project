2ce4bf183405b530398f2c07f57c27966bbd48c7
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-21 23:43:10 +0700

    Add file header of radixSort

--- SOURCE/Algorithms/header/radixSort.hpp
+++ SOURCE/Algorithms/header/radixSort.hpp
@@ -1,8 +1,25 @@
 #pragma once
 #ifndef RADIX_SORT_HPP
 #define RADIX_SORT_HPP
+#include "../../myLibrary.hpp"
+#include "../../Others/header/helpFunctions.hpp"
 
-void radixSort(int array[], int n, long long &count_comparison);
-void radixSort(int array[], int n);
+template<class T>
+T getMax(std::vector<T>& array, int n, long long& count_comparison);
+
+template<class T>
+void countSort(std::vector<T>& array, int n, int exp, long long& count_comparison);
+
+template<class T>
+void radixSort(std::vector<T>& array, int n, long long &count_comparison);
+
+template<class T>
+T getMax(std::vector<T>& array, int n);
+
+template<class T>
+void countSort(std::vector<T>& array, int n, int exp);
+
+template<class T>
+void radixSort(std::vector<T>& array, int n);
 
 #endif // RADIX_SORT_HPP
