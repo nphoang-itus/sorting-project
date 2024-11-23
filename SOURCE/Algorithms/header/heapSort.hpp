55aad248b50cbf997179f4f434bdacda0fdb87f2
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-23 15:42:02 +0700

    Doi kieu du lieu thanh size_t

--- SOURCE/Algorithms/header/heapSort.hpp
+++ SOURCE/Algorithms/header/heapSort.hpp
@@ -6,15 +6,15 @@
 #define HEAP_SORT_HPP
 
 template<class T>
-void heapify(std::vector<T>& array, int n, int i, long long& count_comparison);
+void heapify(std::vector<T>& array, size_t n, size_t i, size_t& count_comparison);
 
 template<class T>
-void heapSort(std::vector<T>& array, int n, long long& count_comparison);
+void heapSort(std::vector<T>& array, size_t& count_comparison);
 
 template<class T>
-void heapify(std::vector<T>& array, int n, int i);
+void heapify(std::vector<T>& array, size_t n, size_t i);
 
 template<class T>
-void heapSort(std::vector<T>& array, int n);
+void heapSort(std::vector<T>& array);
 
 #endif // HEAP_SORT_HPP
