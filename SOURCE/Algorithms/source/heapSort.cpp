61636457ed8eadb196d1f6e5701a1f462788b43f
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-23 15:42:35 +0700

    Doi kieu du lieu thanh size_t va xoa n

--- SOURCE/Algorithms/source/heapSort.cpp
+++ SOURCE/Algorithms/source/heapSort.cpp
@@ -1,11 +1,11 @@
 #include "../header/heapSort.hpp"
 
 template<class T>
-void heapify(std::vector<T>& array, int n, int i, long long& count_comparison)
+void heapify(std::vector<T>& array, size_t n, size_t i, size_t& count_comparison)
 {
     //cài đặt hàm heapify của heapSort
-    int largest = i;                                    //giả sử cho node cha là lớn nhất
-    int left = 2 * i + 1;
+    size_t largest = i;                                    //giả sử cho node cha là lớn nhất
+    size_t left = 2 * i + 1;
     if (++count_comparison && left < n && ++count_comparison && array[largest] < array[left])           //tìm node con lớn nhất mà lớn hơn node cha
     {
         largest = left;
@@ -22,9 +22,10 @@ void heapify(std::vector<T>& array, int n, int i, long long& count_comparison)
 }
 
 template<class T>
-void heapSort(std::vector<T>& array, int n, long long &count_comparison) 
+void heapSort(std::vector<T>& array, size_t& count_comparison) 
 {
     //cài đặt hàm heapSort
+    int n = array.size();
     for (int i = n / 2 - 1; ++count_comparison && i >= 0; i--)                //bắt đầu từ n / 2 - 1 vì đó là phần tử cuối cùng có con
     {
         heapify(array, n, i);                             //Xây dựng max heap
@@ -37,70 +38,71 @@ void heapSort(std::vector<T>& array, int n, long long &count_comparison)
 }
 
 template<class T>
-void heapify(std::vector<T>& arr, int n, int i)
+void heapify(std::vector<T>& array, size_t n, size_t i)
 {
     //cài đặt hàm heapify của heapSort
-    int largest = i;                                    //giả sử cho node cha là lớn nhất
-    int left = 2 * i + 1;
-    if (left < n && arr[largest] < arr[left])           //tìm node con lớn nhất mà lớn hơn node cha
+    size_t largest = i;                                    //giả sử cho node cha là lớn nhất
+    size_t left = 2 * i + 1;
+    if (left < n && array[largest] < array[left])           //tìm node con lớn nhất mà lớn hơn node cha
     {
         largest = left;
     }
-    if (left + 1 < n && arr[largest] < arr[left + 1])
+    if (left + 1 < n && array[largest] < array[left + 1])
     {
         largest = left + 1;
     }
     if (largest != i)                                   //nếu node cha không phải lớn nhất thì đổi chỗ sao cho node cha lớn nhất
     {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);                       //đệ quy xuống node con ở dưới để node con ở dưới lớn nhất
+        swap(array[i], array[largest]);
+        heapify(array, n, largest);                       //đệ quy xuống node con ở dưới để node con ở dưới lớn nhất
     }
 }
 
 template<class T>
-void heapSort(std::vector<T>& arr, int n)
+void heapSort(std::vector<T>& array)
 {
     //cài đặt hàm heapSort
+    int n = array.size();
     for (int i = n / 2 - 1; i >= 0; i--)                //bắt đầu từ n / 2 - 1 vì đó là phần tử cuối cùng có con
     {
-        heapify(arr, n, i);                             //Xây dựng max heap
+        heapify(array, n, i);                             //Xây dựng max heap
     }
     for (int i = n - 1; i > 0; i--)
     {
-        swap(arr[0], arr[i]);                           //đổi chỗ phần tử lớn nhất với phần tử phải cùng mà chưa được sắp xếp
-        heapify(arr, i, 0);                             //heapify tiếp mảng với số phần tử còn lại bắt đầu từ gốc
+        swap(array[0], array[i]);                           //đổi chỗ phần tử lớn nhất với phần tử phải cùng mà chưa được sắp xếp
+        heapify(array, i, 0);                             //heapify tiếp mảng với số phần tử còn lại bắt đầu từ gốc
     }
 }
 
 // INSTANTIATION
 //heapSort
-template void heapify(std::vector<int>&, int, int);
-template void heapSort(std::vector<int>&, int);
+template void heapify(std::vector<int>&, size_t, size_t);
+template void heapSort(std::vector<int>&);
 
-template void heapify(std::vector<long long>&, int, int);
-template void heapSort(std::vector<long long>&, int);
+template void heapify(std::vector<long long>&, size_t, size_t);
+template void heapSort(std::vector<long long>&);
 
-template void heapify(std::vector<double>&, int, int);
-template void heapSort(std::vector<double>&, int);
+template void heapify(std::vector<double>&, size_t, size_t);
+template void heapSort(std::vector<double>&);
 
-template void heapify(std::vector<float>&, int, int);
-template void heapSort(std::vector<float>&, int);
+template void heapify(std::vector<float>&, size_t, size_t);
+template void heapSort(std::vector<float>&);
 
-template void heapify(std::vector<char>&, int, int);
-template void heapSort(std::vector<char>&, int);
+template void heapify(std::vector<char>&, size_t, size_t);
+template void heapSort(std::vector<char>&);
 
 //heapSort with compare
-template void heapify(std::vector<int>&, int, int, long long&);
-template void heapSort(std::vector<int>&, int, long long&);
+template void heapify(std::vector<int>&, size_t, size_t, size_t&);
+template void heapSort(std::vector<int>&, size_t&);
 
-template void heapify(std::vector<long long>&, int, int, long long&);
-template void heapSort(std::vector<long long>&, int, long long&);
+template void heapify(std::vector<long long>&, size_t, size_t, size_t&);
+template void heapSort(std::vector<long long>&, size_t&);
 
-template void heapify(std::vector<double>&, int, int, long long&);
-template void heapSort(std::vector<double>&, int, long long&);
+template void heapify(std::vector<double>&, size_t, size_t, size_t&);
+template void heapSort(std::vector<double>&, size_t&);
 
-template void heapify(std::vector<float>&, int, int, long long&);
-template void heapSort(std::vector<float>&, int, long long&);
+template void heapify(std::vector<float>&, size_t, size_t, size_t&);
+template void heapSort(std::vector<float>&, size_t&);
 
-template void heapify(std::vector<char>&, int, int, long long&);
-template void heapSort(std::vector<char>&, int, long long&);
+template void heapify(std::vector<char>&, size_t, size_t, size_t&);
+template void heapSort(std::vector<char>&, size_t&);
