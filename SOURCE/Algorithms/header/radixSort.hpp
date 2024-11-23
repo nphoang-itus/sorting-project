54d064739685302e9b920d973975852b7bbae9df
Le Tan Hiep <tanhiep24135@gmail.com>
2024-11-23 15:43:15 +0700

    Doi kieu du lieu thanh size_t

--- SOURCE/Algorithms/header/radixSort.hpp
+++ SOURCE/Algorithms/header/radixSort.hpp
@@ -6,21 +6,21 @@
 #define RADIX_SORT_HPP
 
 template<class T>
-T getMax(std::vector<T>& array, int n, long long& count_comparison);
+T getMax(std::vector<T>& array, size_t& count_comparison);
 
 template<class T>
-void countSort(std::vector<T>& array, int n, int exp, long long& count_comparison);
+void countSort(std::vector<T>& array, size_t exp, size_t& count_comparison);
 
 template<class T>
-void radixSort(std::vector<T>& array, int n, long long &count_comparison);
+void radixSort(std::vector<T>& array, size_t& count_comparison);
 
 template<class T>
-T getMax(std::vector<T>& array, int n);
+T getMax(std::vector<T>& array);
 
 template<class T>
-void countSort(std::vector<T>& array, int n, int exp);
+void countSort(std::vector<T>& array, size_t exp);
 
 template<class T>
-void radixSort(std::vector<T>& array, int n);
+void radixSort(std::vector<T>& array);
 
 #endif // RADIX_SORT_HPP
