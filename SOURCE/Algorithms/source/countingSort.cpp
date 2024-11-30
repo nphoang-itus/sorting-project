b9724b8a91969eba840b6cc6c55c34480dd9dac6
Nguyen Phuc Hoang <nphuchoang.itus@gmail.com>
2024-11-30 15:38:11 +0700

    update counting sort

--- SOURCE/Algorithms/source/countingSort.cpp
+++ SOURCE/Algorithms/source/countingSort.cpp
@@ -1,9 +1,52 @@
 #include "../header/countingSort.hpp"
 
-void countingSort(int array[], int n, long long &count_comparison) {
+template <class T>
+void countingSort(std::vector<T> &array) {
+    std::memset(cnt, 0, sizeof(cnt));
+    int maxValue = INT_MIN;
 
+    for (int i = 0; i < array.size(); ++i) {
+        cnt[array[i]]++;
+        maxValue = max(maxValue, array[i]);
+    }
+
+    int idx = 0;
+    for (int i = 0; i <= maxValue; ++i) {
+        for (; cnt[i]; --cnt[i], l++) {
+            array[l] = i;
+        }
+    }
 }
 
-void countingSort(int array[], int n) {
+template <class T>
+void countingSort(std::vector<T> &array, long long &count_comparison) {
+    count_comparison = 0;
 
+    std::memset(cnt, 0, sizeof(cnt));
+    int maxValue = INT_MIN;
+
+    for (int i = 0; ++count_comparison && i < array.size(); ++i) {
+        cnt[array[i]]++;
+        maxValue = max(maxValue, array[i], count_comparison);
+    }
+
+    int idx = 0;
+    for (int i = 0; ++count_comparison && i <= maxValue; ++i) {
+        for (; ++count_comparison && cnt[i]; --cnt[i], l++) {
+            array[l] = i;
+        }
+    }
 }
+
+// INSTANTIATION
+// Counting Sort
+template void countingSort(std::vector<int>&);
+template void countingSort(std::vector<long long>&);
+template void countingSort(std::vector<float>&);
+template void countingSort(std::vector<double>&);
+
+// Counting Sort with Com
+template void countingSort(std::vector<int>&, long long&);
+template void countingSort(std::vector<long long>&, long long&);
+template void countingSort(std::vector<float>&, long long&);
+template void countingSort(std::vector<double>&, long long&);
