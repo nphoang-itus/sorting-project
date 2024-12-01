267edee8ab4945a5ed1e99ea77172c8633b3695a
Le Tan Hiep <tanhiep24135@gmail.com>
2024-12-01 15:48:35 +0700

    Add file header of binaryInsertionSort

--- SOURCE/Algorithms/header/binaryInsertionSort.hpp
+++ SOURCE/Algorithms/header/binaryInsertionSort.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
+#include "../../Others/header/helpFunctions.hpp"
+
 #ifndef BINARY_INSERTION_SORT_HPP
 #define BINARY_INSERTION_SORT_HPP
 
+template<class T>
+size_t binarySearch(std::vector<T>& array, T target, size_t low, size_t high);
+
 template <class T>
-void binaryInsertionSort(std::vector<T>& array);
+void binaryInsertionSort (std::vector<T>& array);
+
+template<class T>
+size_t binarySearch(std::vector<T>& array, T target, size_t low, size_t high, size_t& count_comparison);
+
 template <class T>
-void binaryInsertionSort(std::vector<T> &array, long long &countComparison);
+void binaryInsertionSort (std::vector<T>& array, size_t& count_comparison);
 
 #endif // BINARYINSERTION_SORT_HPP
