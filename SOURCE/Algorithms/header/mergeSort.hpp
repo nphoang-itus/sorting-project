28ca7646346bc5212098ae3d78039e852922b41d
Le Tan Hiep <tanhiep24135@gmail.com>
2024-12-01 11:36:37 +0700

    Add file header of mergeSort

--- SOURCE/Algorithms/header/mergeSort.hpp
+++ SOURCE/Algorithms/header/mergeSort.hpp
@@ -1,8 +1,25 @@
 #pragma once
+
+#include "../../Others/header/helpFunctions.hpp"
+
 #ifndef MERGE_SORT_HPP
 #define MERGE_SORT_HPP
 
-void mergeSort(int array[], int n, long long &count_comparison);
-void mergeSort(int array[], int n);
+template<class T>
+void Merge(std::vector<T>& array, size_t left, size_t mid, size_t right, size_t& count_comparison);
 
+template<class T>
+void impleMergeSort(std::vector<T>& array, size_t left, size_t right, size_t& count_comparison);
+
+template<class T>
+void mergeSort(std::vector<T>& array, size_t& count_comparison);
+
+template<class T>
+void Merge(std::vector<T>& array, size_t left, size_t mid, size_t right);
+
+template<class T>
+void impleMergeSort(std::vector<T>& array, size_t left, size_t right);
+
+template<class T>
+void mergeSort(std::vector<T>& array);
 #endif // MERGE_SORT_HPP
