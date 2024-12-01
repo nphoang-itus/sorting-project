838e1ffc52e76e1de26b00f87fcf7ef2f6ce4288
Le Tan Hiep <tanhiep24135@gmail.com>
2024-12-02 01:50:48 +0700

    Add file header of quickSort

--- SOURCE/Algorithms/header/quickSort.hpp
+++ SOURCE/Algorithms/header/quickSort.hpp
@@ -1,8 +1,26 @@
 #pragma once
+
+#include "../../Others/header/helpFunctions.hpp"
+
 #ifndef QUICK_SORT_HPP
 #define QUICK_SORT_HPP
 
-void quickSort(int array[], int n, long long &count_comparison);
-void quickSort(int array[], int n);
+template<class T>
+int Partition(std::vector<int>& array, int low, int high, size_t& count_comparison);
+
+template<class T>
+void impleQuickSort(std::vector<T>& array, int low, int high, size_t& count_comparison);
+
+template<class T>
+void quickSort(std::vector<T>& array, size_t& count_comparison);
+
+template<class T>
+int Partition(std::vector<int>& array, int low, int high);
+
+template<class T>
+void impleQuickSort(std::vector<T>& array, int low, int high);
+
+template<class T>
+void quickSort(std::vector<T>& array);
 
 #endif // QUICK_SORT_HPP
