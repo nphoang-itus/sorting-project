bd9a9fbc2119bfd3627fb848a4e4fc55e597a6d7
Le Tan Hiep <tanhiep24135@gmail.com>
2024-12-04 23:39:01 +0700

    Them comment

--- SOURCE/Algorithms/source/countingSort.cpp
+++ SOURCE/Algorithms/source/countingSort.cpp
@@ -3,29 +3,29 @@
 template<class T>
 void countingSort(std::vector<T>& array)
 {
-    T Max = 0;
+    T Max = 0;                              //lấy số lớn nhất
     size_t n = array.size();
     for (size_t i = 0; i < n; i++)
     {
         if (array[i] > Max)
         {
-            Max = array[i];
+            Max = array[i];                 //tìm số lớn nhất trong mảng
         }
     }
-    std::vector<T> frequency(Max + 1, 0);
+    std::vector<T> frequency(Max + 1, 0);   //tạo mảng tần số có chỉ số từ 0 đến số lớn nhất đó
     for (size_t i = 0; i < n; i++)
     {
-        frequency[array[i]]++;
+        frequency[array[i]]++;              //đếm tần số với frequency[i] = x thì i là số còn x là tần số của i trong mảng
     }
     for (size_t i = 1; i <= Max; i++)
     {
-        frequency[i] += frequency[i - 1];
+        frequency[i] += frequency[i - 1];   //Tạo mảng cộng dồn từ mảng tần số ban đầu để lấy vị trí cuối cùng của phần tử xuất hiện
     }
     std::vector<T> temp(n);
     for (int i = n - 1; i >= 0; i--)
     {
-        temp[frequency[array[i]] - 1] = array[i];
-        frequency[array[i]]--;
+        temp[frequency[array[i]] - 1] = array[i];   //duyệt ngược mảng để đưa số ở sau về vị trí đúng (điều làm cho stable)
+        frequency[array[i]]--; 
     }
 
     array = temp;
@@ -33,29 +33,29 @@ void countingSort(std::vector<T>& array)
 
 template<class T>
 void countingSort(std::vector<T>& array, size_t& count_comparison)
-{
-    T Max = 0;
+{   
+    T Max = 0;                                                  //lấy số lớn nhất
     size_t n = array.size();
     for (size_t i = 0; ++count_comparison && i < n; i++)
     {
         if (++count_comparison && array[i] > Max)
         {
-            Max = array[i];
+            Max = array[i];                                      //tìm số lớn nhất trong mảng
         }
     }
-    std::vector<T> frequency(Max + 1, 0);
+    std::vector<T> frequency(Max + 1, 0);                       //tạo mảng tần số có chỉ số từ 0 đến số lớn nhất đó
     for (size_t i = 0; ++count_comparison && i < n; i++)
     {
-        frequency[array[i]]++;
+        frequency[array[i]]++;                                  //đếm tần số với frequency[i] = x thì i là số còn x là tần số của i trong mảng
     }
     for (size_t i = 1; ++count_comparison && i <= Max; i++)
     {
-        frequency[i] += frequency[i - 1];
+        frequency[i] += frequency[i - 1];                     //Tạo mảng cộng dồn từ mảng tần số ban đầu để lấy vị trí cuối cùng của phần tử xuất hiện
     }
     std::vector<T> temp(n);
     for (int i = n - 1; ++count_comparison && i >= 0; i--)
     {
-        temp[frequency[array[i]] - 1] = array[i];
+        temp[frequency[array[i]] - 1] = array[i];              //duyệt ngược mảng để đưa số ở sau về vị trí đúng (điều làm cho stable)
         frequency[array[i]]--;
     }
     array = temp;
